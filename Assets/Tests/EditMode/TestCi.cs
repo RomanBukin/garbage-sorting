3bf86601e63a0dcd12c56f2177bc52af7a5e59a9
Roman Bukin <bukin.roma@gmail.com>
2022-05-28 22:16:56 +0300

    Added sample tests.

---
+++ Assets/Tests/EditMode/TestCi.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.EditMode
+{
+    public class TestCi
+    {
+        [Test]
+        public void TestCiSimplePasses()
+        {
+            Assert.True(true);
+            
+            Debug.Log("Debug log");
+            Console.WriteLine("Console write line");
+            
+            Assert.False(true);
+        }
+    }
+}
