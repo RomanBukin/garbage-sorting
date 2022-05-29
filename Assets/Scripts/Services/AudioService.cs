20ce0bedd2844b25251e18cb284fe71417092b2a
Roman Bukin <bukin.roma@gmail.com>
2022-05-29 11:47:46 +0300

    Added Audio Service.

---
+++ Assets/Scripts/Services/AudioService.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace Services
+{
+    [RequireComponent(typeof(AudioListener))]
+    public class AudioService : MonoBehaviour
+    {
+        [SerializeField] private AudioSource musicAudioSource;
+        [SerializeField] private AudioSource soundAudioSource;
+        
+        [Header("Audio clips")]
+        [SerializeField] private AudioClip menuMusic;
+        [SerializeField] private AudioClip[] gameplayMusic;
+        [SerializeField] private AudioClip[] sounds;
+
+        // public bool IsPlaying => _audioSource.isPlaying;
+
+        public void PlayMenuMusic()
+        {
+            PlayMusicAudioClip(menuMusic);
+        }
+    
+        public void PlayGameplayMusic(int index)
+        {
+            if (index < gameplayMusic.Length)
+            {
+                PlayMusicAudioClip(gameplayMusic[index]);
+            }
+        }
+
+        private void PlayMusicAudioClip(AudioClip clip)
+        {
+            musicAudioSource.clip = clip;
+            musicAudioSource.Play();
+        }
+
+        public void StopMusic()
+        {
+            musicAudioSource.Stop();
+        }
+
+        public void PlaySound(int index)
+        {
+            if (index < sounds.Length)
+            {
+                soundAudioSource.PlayOneShot(sounds[index]);
+            }
+        }
+    }
+}
