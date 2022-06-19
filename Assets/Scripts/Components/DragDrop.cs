5daabe5e857d9a5326cb9daa01e502176f21bd45
Roman Bukin <bukin.roma@gmail.com>
2022-06-19 10:22:52 +0300

    Fix dragged on top.

--- Assets/Scripts/Components/DragDrop.cs
+++ Assets/Scripts/Components/DragDrop.cs
@@ -1,41 +1,50 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class DragDrop : MonoBehaviour
+namespace Components
 {
-    private Rigidbody2D _rigidbody2D;
-    private bool _dragged;
-    private Camera _camera;
-    private Transform _transform;
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class DragDrop : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody2D;
+        private bool _dragged;
+        private Camera _camera;
+        private Transform _transform;
+        private float _positionZ;
 
-    public void Awake()
-    {
-        _camera = Camera.main;
-        _transform = transform;
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+        private const int DraggedPositionZ = -6;
 
-    public void OnMouseDown()
-    {
-        _rigidbody2D.simulated = false;
-        _rigidbody2D.velocity = Vector2.zero;
-        _dragged = true;
-    }
-    
-    public void OnMouseUp()
-    {
-        _rigidbody2D.simulated = true;
-        _dragged = false;
-    }
-
-    public void Update()
-    {
-        if (!_dragged)
+        public void Awake()
         {
-            return;
+            _camera = Camera.main;
+            _transform = transform;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-        var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        _transform.position = new Vector3(mousePosition.x, mousePosition.y, _transform.position.z);
+        public void OnMouseDown()
+        {
+            _positionZ = _transform.position.z;
+            _rigidbody2D.simulated = false;
+            _rigidbody2D.velocity = Vector2.zero;
+            _dragged = true;
+        }
+    
+        public void OnMouseUp()
+        {
+            var position = _transform.position;
+            _transform.position = new Vector3(position.x, position.y, _positionZ);
+            _rigidbody2D.simulated = true;
+            _dragged = false;
+        }
+
+        public void Update()
+        {
+            if (!_dragged)
+            {
+                return;
+            }
+
+            var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _transform.position = new Vector3(mousePosition.x, mousePosition.y, DraggedPositionZ);
+        }
     }
 }
